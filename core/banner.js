d0b12a93a6120077e1fbf38e96fe718013fdeb8a
Mahesh <136630971+Learnwisehub@users.noreply.github.com>
2025-05-28 14:46:02 +0545

    Add files via upload

---
+++ core/banner.js
@@ -0,0 +1,6 @@
+const colors = require("colors");
+function showBanner() {
+  console.log(colors.yellow("Tool Shared By FOREST ARMY (https://t.me/airdropscriptFA)"));
+}
+
+module.exports = { showBanner };
